cf85a9dd73ddbdd36d4fddba42ffb420734b2d5f
vincent guyader <vincent@guyader.pro>
2017-12-10 15:10:55 +0100

    Update install-mini.R

--- install-mini.R
+++ install-mini.R
@@ -24,7 +24,10 @@ packs <- installed.packages()
 exc <- names(packs[,'Package'])
   local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org/"; options(repos=r)})
   # update.packages(ask=FALSE)
-  ainstaller<-unique(sort(c("abind","arsenal", "abn", "bbmle", "bit64", "CADFtest","rmdformats","writexl", "car", "chron", 
+  ainstaller<-unique(sort(c("abind","arsenal", "abn", "bbmle", "bit64", "CADFtest","rmdformats","writexl",
+                            "pryr","ggraph","togglr","shinyBS",
+                            
+                            "car", "chron", 
                        "class", "cluster", "colorspace", "compiler", "data.table", "DBI", 
                        "devEMF", "devtools", "dichromat", "digest", "doSNOW", "dplyr","dygraphs", 
                        "e1071", "ellipse", "evaluate", "explor", "FactoMineR", "Factoshiny", "plotly",
