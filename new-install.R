ab26347dfee960ce35042e30626c9af69247b239
colin <colin@thinkr.fr>
2020-03-24 15:39:09 +0100

    new install

---
+++ new-install.R
@@ -0,0 +1,276 @@
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(
+    formatC(
+      100 * x, 
+      format = format, 
+      digits = digits, 
+      ...
+    ), 
+    "%"
+  )
+}
+
+mon_print <- function(synth){
+  N <- nrow(synth)
+  if( is.null(N) ){
+    N <- 1
+    synth <- matrix(
+      synth,
+      nrow=1
+    )
+  }
+  
+  for ( i in 1:N){
+    tp <- synth[i,]
+    tp[1]  <-stringr::str_pad(tp[1],15,side="right")
+    cat(tp,"\n")
+  }
+}
+
+packs <- as.data.frame(installed.packages())
+if (! "remotes" %in% packs$Package ){
+  install.packages("remotes")
+}
+remotes::install_cran("withr")
+remotes::install_cran("progress")
+remotes::install_cran("cli")
+remotes::install_version("attempt", "0.3.0")
+
+to_install <- unique(
+  sort(
+    c(
+      "abind",
+      "abn",
+      "arsenal", 
+      "attachment", 
+      "attempt", 
+      "bbmle",
+      "bit64", 
+      "broom", 
+      "car", 
+      "chron", 
+      "class", 
+      "cluster", 
+      "colorspace",
+      "compiler",
+      "data.table", 
+      "DBI", 
+      "devEMF", 
+      "devtools", 
+      "dichromat",
+      "digest", 
+      "doSNOW", 
+      "dplyr", 
+      "DT", 
+      "dygraphs", 
+      "e1071", 
+      "ellipse",
+      "evaluate", 
+      "explor", 
+      "FactoMineR",
+      "fcuk", 
+      "forecast",
+      "foreign", 
+      "formatR", 
+      "Formula", 
+      "fortunes", 
+      "gapminder", 
+      "gbm",
+      "ggplot2", 
+      "ggraph", 
+      "ggthemes", 
+      "golem", 
+      "gplots", 
+      "graphics",
+      "grDevices",
+      "grid", 
+      "gtable", 
+      "h2o", 
+      "haven", 
+      "hflights", 
+      "highr",
+      "Hmisc", 
+      "htmltools", 
+      "httr", 
+      "icarus", 
+      "jpeg", 
+      "knitr", 
+      "labeling",
+      "lattice", 
+      "latticeExtra",
+      "leaps",
+      "markdown", 
+      "MASS", 
+      "methods",
+      "microbenchmark", 
+      "mime", "munsell", 
+      "nnet", 
+      "nycflights13",
+      "openxlsx", 
+      "packrat", 
+      "pkgbuild", 
+      "pkgconfig", 
+      "pkgload", 
+      "plotly",
+      "proto", 
+      "proustr",
+      "pryr", 
+      "R6", 
+      "randomForest", 
+      "raster", 
+      "RColorBrewer",
+      "Rcpp", 
+      "readr",
+      "readxl", 
+      "remedy",
+      "reprex",
+      "reshape", 
+      "reshape2",
+      "rJava",
+      "rmarkdown",
+      "rmdformats",
+      "roxygen2", 
+      "rpart", 
+      "rusk", 
+      "flextable",
+      "rvest", 
+      "sandwich", 
+      "sas7bdat", 
+      "scales", 
+      "scatterplot3d", 
+      "shinyBS",
+      "shinydashboard", 
+      "shinyWidgets", 
+      "skimr", 
+      "splines", 
+      "sqldf",
+      "stats", 
+      "stringr", 
+      "survival", 
+      "tcltk", 
+      "tcltk2", 
+      "thinkr",
+      "tidystringdist", 
+      "tidytext",
+      "tidyverse", 
+      "togglr",
+      "tools",
+      "topicmodels",
+      "tseries", 
+      "usethis", 
+      "utils", 
+      "vroom", 
+      "writexl",
+      "xaringan", 
+      "XLConnect",
+      "XLConnectJars", 
+      "xtable", 
+      "xts", 
+      "zoo", 
+      "learnr"
+    )
+  )
+)
+
+failed <- c()
+success <- c()
+
+withr::with_options(
+  c(repos =  "http://cran.r-project.org/"), {
+    cli:cat_rule("Starting packages installation")
+    
+    library(progress)
+    pb <- progress_bar$new(total = length(to_install))
+    
+    for (i in seq_along(to_install)){
+      
+      cli::cat_bullet(
+        sprintf(
+          "Starting %s installation", 
+          i
+        ), bullet = "play"
+      )
+      
+      tst <- attempt::attempt({
+        remotes::install_cran(i, quiet = TRUE)
+      })
+      if (attempt::is_try_error(tst)){
+        cli::cat_bullet(
+          sprintf(
+            "Error installing %s", i
+          ), 
+          bullet = "cross"
+        )
+        failed <- c(failed, i)
+      } else {
+        cli::cat_bullet(
+          sprintf(
+            "%s installed", i
+          ), 
+          bullet = "tick"
+        )
+        success <- c(success, i)
+      }
+    }
+    
+  }
+)
+
+cli::cat_rule("Ended CRAN Installation. Starting GitHub Installation.")
+
+to_install <- c(
+  "ThinkR-open/prenoms", 
+  "ThinkR-open/shopping"
+)
+
+withr::with_options(
+  c(repos =  "http://cran.r-project.org/"), {
+    cli:cat_rule("Starting packages installation")
+    
+    library(progress)
+    pb <- progress_bar$new(total = length(to_install))
+    
+    for (i in seq_along(to_install)){
+      
+      cli::cat_bullet(
+        sprintf(
+          "Starting %s installation", 
+          i
+        ), bullet = "play"
+      )
+      
+      tst <- attempt::attempt({
+        remotes::install_github(
+          i,
+          upgrade = "always", 
+          quiet = TRUE
+        )
+      })
+      if (attempt::is_try_error(tst)){
+        cli::cat_bullet(
+          sprintf(
+            "Error installing %s", i
+          ), 
+          bullet = "cross"
+        )
+        failed <- c(failed, i)
+      } else {
+        cli::cat_bullet(
+          sprintf(
+            "%s installed", i
+          ), 
+          bullet = "tick"
+        )
+        success <- c(success, i)
+      }
+    }
+    
+  }
+)
+
+cli::cat_line()
+cli::cat_line()
+cli::cat_rule("The following package(s) have been installed:")
+cli::cat_bullet(failed, bullet="tick")
+cli::cat_rule("The following package(s) failed to install:")
+cli::cat_bullet(success, bullet="cross")
