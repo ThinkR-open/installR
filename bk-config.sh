2bd70445c3cbcd9b0786e41e206eb8f6cf9a8a28
colin <colin@thinkr.fr>
2021-12-20 09:42:58 +0100

    bk

---
+++ bk-config.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+add-apt-repository --enable-source --yes "ppa:marutter/rrutter3.5"
+add-apt-repository --enable-source --yes "ppa:marutter/c2d4u3.5"
+
+# Spatial stuff
+apt-get update \
+  && apt-get install -y --no-install-recommends \
+    lbzip2 \
+    libfftw3-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libgsl0-dev \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libhdf4-alt-dev \
+    libhdf5-dev \
+    libmagick++-dev \
+    libjq-dev \
+    liblwgeom-dev \
+    libpq-dev \
+    libproj-dev \
+    libprotobuf-dev \
+    libnetcdf-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libudunits2-dev \
+    netcdf-bin \
+    postgis \
+    protobuf-compiler \
+    sqlite3 \
+    tk-dev \
+    unixodbc-dev \
+    imagemagick
+
+apt-get install -y \
+    libmagick++-dev \
+    libjq-dev  \
+    libv8-dev  \
+    libprotobuf-dev  \
+    protobuf-compiler  \
+    libsodium-dev  \
+    imagemagick \
+    libgit2-dev
+
+# database ----
+apt-get update \
+    apt-get install -y --no-install-recommends \
+  default-mysql-client \
+  default-libmysqlclient-dev  
+
+# Locals
+apt-get install -y language-pack-fr
+mv /etc/localtime /etc/localtime_backup \
+  && ln -s /usr/share/zoneinfo/Europe/Paris
+
+# Chromium for {pagedown} ----
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
+apt-get install -y gnupg2 \
+  && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update
+apt-get -y install libxpm4 \
+    libxrender1 \
+    libgtk2.0-0 \
+    libnss3 \
+    libgconf-2-4 \
+    xvfb \
+    gtk2-engines-pixbuf \
+    xfonts-cyrillic \
+    xfonts-100dpi \
+    xfonts-75dpi \
+    xfonts-base \
+    xfonts-scalable \
+    google-chrome-stable
+
---
+++ bk-install.R
@@ -0,0 +1,274 @@
+options(
+  repos = c(
+    REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest/"
+  ),
+  Ncpus = 6
+)
+
+update.packages(ask = FALSE)
+
+install.packages("pak")
+
+pak::pkg_install("progress")
+pak::pkg_install("cli")
+
+to_install <- unique(
+  sort(
+    c(
+      "abind",
+      "abn",
+      "arsenal",
+      "attachment",
+      "attempt",
+      "bbmle",
+      "bit64",
+      "broom",
+      "bookdown",
+      "car",
+      "chron",
+      "class",
+      "cluster",
+      "colorspace",
+      "compiler",
+      "data.table",
+      "DBI",
+      "devEMF",
+      "devtools",
+      "dichromat",
+      "digest",
+      "doSNOW",
+      "dplyr",
+      "DT",
+      "dygraphs",
+      "e1071",
+      "ellipse",
+      "evaluate",
+      "explor",
+      "FactoMineR",
+      "fcuk",
+      "forecast",
+      "foreign",
+      "formatR",
+      "Formula",
+      "fortunes",
+      "fusen",
+      "gapminder",
+      "gbm",
+      "ggplot2",
+      "ggraph",
+      "ggthemes",
+      "golem",
+      "gplots",
+      "graphics",
+      "grDevices",
+      "grid",
+      "gtable",
+      "h2o",
+      "haven",
+      "hflights",
+      "highr",
+      "Hmisc",
+      "htmltools",
+      "httr",
+      "icarus",
+      "jpeg",
+      "knitr",
+      "labeling",
+      "lattice",
+      "latticeExtra",
+      "leaps",
+      "markdown",
+      "MASS",
+      "methods",
+      "microbenchmark",
+      "mime", "munsell",
+      "nnet",
+      "nycflights13",
+      "openxlsx",
+      "packrat",
+      "pander",
+      "pkgbuild",
+      "pkgconfig",
+      "pkgload",
+      "plotly",
+      "proto",
+      "proustr",
+      "pryr",
+      "R6",
+      "randomForest",
+      "raster",
+      "RColorBrewer",
+      "Rcpp",
+      "readr",
+      "readxl",
+      "remedy",
+      "rstudio/renv",
+      "reprex",
+      "reshape",
+      "reshape2",
+      "rJava",
+      "rmarkdown",
+      "rmdformats",
+      "roxygen2",
+      "rpart",
+      "rusk",
+      "flextable",
+      "rvest",
+      "sandwich",
+      "sas7bdat",
+      "scales",
+      "scatterplot3d",
+      "shinyBS",
+      "shinydashboard",
+      "shinyWidgets",
+      "skimr",
+      "spelling",
+      "splines",
+      "sqldf",
+      "stats",
+      "stringr",
+      "styler",
+      "survival",
+      "tcltk",
+      "tcltk2",
+      "thinkr",
+      "tidystringdist",
+      "tidytext",
+      "tidyverse",
+      "togglr",
+      "tools",
+      "topicmodels",
+      "tseries",
+      "usethis",
+      "utils",
+      "vroom",
+      "webshot",
+      "writexl",
+      "xaringan",
+      "XLConnect",
+      "XLConnectJars",
+      "xtable",
+      "xts",
+      "zoo",
+      "learnr",
+      "pagedown",
+      "tinytex",
+      "ThinkR-open/prenoms",
+      "ThinkR-open/shopping",
+      "sf",
+      "leaflet",
+      "raster",
+      "terra",
+      "rosm",
+      "gstat",
+      "maps",
+      "mapdata",
+      "ncdf4",
+      "sp",
+      "geoR",
+      "ggmap",
+      "here",
+      "Rcpp",
+      "units",
+      "lwgeom",
+      "stars",
+      "ggspatial",
+      "extrafont",
+      "ggrepel",
+      "tmap",
+      "rnaturalearth",
+      "rnaturalearthdata",
+      "gganimate",
+      "terra",
+      "ceramic",
+      "mapview",
+      "mapedit",
+      "rgl",
+      "rgeos",
+      "magick",
+      "colourpicker",
+      "cartography",
+      "shinyjs",
+      "transformr",
+      "geofacet",
+      "mapdeck",
+      "ropensci/rnaturalearthhires",
+      "statnmap/cartomisc",
+      "remotes",
+      "thinkr-open/golem@master"
+    )
+  )
+)
+
+failed <- c()
+success <- c()
+
+cli::cat_rule("Starting packages installation")
+
+library(progress)
+pb <- progress_bar$new(total = length(to_install))
+
+for (i in seq_along(to_install)) {
+  pb$tick()
+
+  cli::cat_line()
+
+  pak <- to_install[i]
+
+  cli::cat_bullet(
+    sprintf(
+      "Starting %s installation",
+      pak
+    ),
+    bullet = "play"
+  )
+
+  tst <- attempt::attempt({
+    pak::pkg_install(
+      pak,
+      quiet = TRUE
+    )
+  })
+
+  if (attempt::is_try_error(tst)) {
+    cli::cat_bullet(
+      sprintf(
+        "Error installing %s",
+        pak
+      ),
+      bullet = "cross"
+    )
+    failed <- c(failed, pak)
+  } else {
+    cli::cat_bullet(
+      sprintf(
+        "%s installed", pak
+      ),
+      bullet = "tick"
+    )
+    success <- c(success, pak)
+  }
+}
+
+cli::cat_rule("Installation ended.")
+
+cli::cat_line()
+cli::cat_line()
+cli::cat_rule("The following package(s) have been installed:")
+cli::cat_bullet(success, bullet = "tick")
+cli::cat_line()
+cli::cat_rule("The following package(s) failed to install:")
+cli::cat_bullet(failed, bullet = "cross")
+
+cli::cat_line()
+cli::cat_rule("Installing webshot")
+webshot::install_phantomjs()
+system("cp ~/bin/phantomjs /usr/local/share/phantomjs")
+system("chmod 0755 /usr/local/share/phantomjs")
+system("ln -sf /usr/local/share/phantomjs /usr/local/bin")
+cli::cat_bullet("PhantomJS installed", bullet = "tick")
+
+cli::cat_line()
+cli::cat_rule("Installing tinytex")
+tinytex::install_tinytex()
+cli::cat_bullet("tinytex installed", bullet = "tick")
